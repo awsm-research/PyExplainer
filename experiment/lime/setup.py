7e98ad479440717424dd7d1d84f24413024f88c2
oathaha <windowsandandroid.app@gmail.com>
2021-04-16 17:10:37 +1000

    fix LIME not in repo

---
+++ experiment/lime/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(name='lime',
+      version='0.2.0.1',
+      description='Local Interpretable Model-Agnostic Explanations for machine learning classifiers',
+      url='http://github.com/marcotcr/lime',
+      author='Marco Tulio Ribeiro',
+      author_email='marcotcr@gmail.com',
+      license='BSD',
+      packages=find_packages(exclude=['js', 'node_modules', 'tests']),
+      python_requires='>=3.5',
+      install_requires=[
+          'matplotlib',
+          'numpy',
+          'scipy',
+          'tqdm >= 4.29.1',
+          'scikit-learn>=0.18',
+          'scikit-image>=0.12',
+          'pyDOE2==1.3.0'
+      ],
+      extras_require={
+          'dev': ['pytest', 'flake8'],
+      },
+      include_package_data=True,
+      zip_safe=False)
