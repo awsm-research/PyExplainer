db57c341d71417acac7ea8bf0a0f2ba2e8298278
Michael <michaelfu1998@gmail.com>
2021-07-15 21:44:48 +1000

    pyexp 0.2.2 (fix dependency & change gif file)
    fix "pandas" dependency issue by restricting the dep version to 1.2.5
    
    change gif file to have slower demo

--- tests/test_pyexplainer.py
+++ tests/test_pyexplainer.py
@@ -120,7 +120,7 @@ testing_risk_data = py_explainer.generate_risk_data(py_explainer.X_explain)
 
 
 def test_version():
-    assert __version__ == '0.2.0'
+    assert __version__ == '0.2.2'
 
 
 @pytest.mark.parametrize('data, result',
